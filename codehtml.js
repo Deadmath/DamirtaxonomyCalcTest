1965d76b4b9859e64c988a48a6299a1565b19d56
Deadmath <ddd982379@gmail.com>
2024-08-23 20:30:15 -0400

    Add files via upload

---
+++ codehtml.js
@@ -0,0 +1,44 @@
+/// import*Element~npm install mahalanobis
+/// Calculate saved data variables
+/// Calculate Cosine similarity
+
+
+// Create elements $ [HTML].js => div
+
+/// first click on function
+
+
+
+const euclideanDistance = (a, b) =>
+   Math.hypot(...Object.keys(a).map(k => b[k] - a[k]));
+
+function euclidean() { //#text&number
+
+
+   
+
+
+  var PersonD2,D1,D2,Res1,Res2;
+ 
+  
+  
+  D1=Number(document.Sim.p46fig3.value);
+  D2=Number(document.Sim.p46fig3.value);
+  PersonD2=Number(document.Sim.You.value);
+
+
+  Res1= Math.hypot(PersonD2,D1)
+  document.Sim.Res12.value=Res1;
+  
+  Res2=Math.hypot(PersonD2,D2)
+  document.Sim.Res22.value=Res2;
+
+  console.log(Res1);
+  console.log(Res2);
+   
+  String(true);
+
+
+
+
+}
